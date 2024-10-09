7cc634b9c12c372cd28e2a413356644ab0d14503
root <root@isv-registry.susealliances.com>
2024-10-08 22:25:44 -0600

    updates

---
+++ set-up-metallb/01-edit-then-set-variables.cmd
@@ -0,0 +1,21 @@
+# Set these variables then run this file
+
+
+echo '
+
+export APPLICATION_EXTERNAL_IP="192.168.150.175"
+export APPLICATION_NAME="suse-private-ai"
+export APPLICATION_NAMESPACE="suse-private-ai"
+export AWS_ACCESS_KEY_ID="AKIA36U5DH36DUBJQO45"
+export AWS_REGION="us-west-2"
+export DEFAULT_IP_RANGE_START=""
+export DEFAULT_IP_RANGE_END=""
+export DNSZONE="susealliances.com"
+export EMAIL_ADDR="alex.arnoldy@suse.com"
+export FQDN="suse-private-ai.susealliances.com"
+
+' > /tmp/variables
+
+grep -v ^$ /tmp/variables > /tmp/variables-1
+
+mv /tmp/variables-1 /tmp/variables
