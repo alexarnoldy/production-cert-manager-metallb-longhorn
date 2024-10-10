5bd120fab9d0944bac8dcc7d8c9f1903400c24f3
root <root@isv-registry.susealliances.com>
2024-10-10 09:31:07 -0600

    updates

--- set-up-metallb/08-test-metallb-service.cmd
+++ set-up-metallb/08-test-metallb-service.cmd
@@ -4,12 +4,12 @@ read -r a b c EXTERNAL_IP e f < /tmp/externalip
 
 NODE_COUNT=$(kubectl get nodes --no-headers | wc -l)
 
-COUNTER=-2
+COUNTER=-${NODE_COUNT}
 
 while [ $COUNTER -lt $NODE_COUNT ]; do
   COUNTER=$((COUNTER + 1))
   curl http://${EXTERNAL_IP}:8080 
-#  curl http://${c}:8080 
+#  curl http://${c}:8080    ## Use for testing when a load balanced, or another, ExternalIP is not available
   done
 
 rm /tmp/externalip
