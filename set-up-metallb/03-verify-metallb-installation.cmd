388912a88ee5d22d932c272b4fe357045fb5a5bd
alexarnoldy <alexarnoldy@gmail.com>
2024-10-21 11:56:09 -0600

    updates

--- set-up-metallb/03-verify-metallb-installation.cmd
+++ set-up-metallb/03-verify-metallb-installation.cmd
@@ -1,2 +1,9 @@
 kubectl -n metallb-system get all 
+
+echo ""
+
+echo -e "\033[35;4mUse Ctrl+c to continue when the all of the pods are running\033[0m"
+
+echo ""
+
 kubectl -n metallb-system get pods -w
--- set-up-metallb/08-test-metallb-service.cmd
+++ set-up-metallb/08-test-metallb-service.cmd
@@ -2,17 +2,23 @@ source /tmp/variables && \
 kubectl get svc -n default | grep -w nginx > /tmp/externalip
 read -r a b c EXTERNAL_IP e f < /tmp/externalip
 
-NODE_COUNT=$(kubectl get nodes --no-headers | wc -l)
+#NODE_COUNT=$(kubectl get nodes --no-headers | wc -l)
 
-COUNTER=-${NODE_COUNT}
+#COUNTER=-${NODE_COUNT}
 
-while [ $COUNTER -lt $NODE_COUNT ]; do
-  COUNTER=$((COUNTER + 1))
-#  curl http://${EXTERNAL_IP}:8080 
-  curl http://${c}:8080    ## Use for testing when a load balanced, or another, ExternalIP is not available
+echo ""
+echo -e "\033[35;4mContinue when each node running a pod has responded at least once.\033[0m"
+echo ""
+
+sleep 5
+
+ while true; do
+# while [ $COUNTER -lt $NODE_COUNT ]; do
+#  COUNTER=$((COUNTER + 1))
+  curl http://${EXTERNAL_IP}:8080 
+#  curl http://${c}:8080    ## Use for testing when a load balanced, or other ExternalIP is not available
+  sleep 2
   done
 
 rm /tmp/externalip
 
-echo ""
-echo -e "\033[35;4mContinue when each node running a pod has responded at least once.\033[0m"
